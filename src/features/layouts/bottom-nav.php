ab47e2d116d781a885fcfa3e2d54933676f0d334
TeddieXII <benard.abigail001@gmail.com>
2026-03-22 23:53:43 +0400

    Add nav bottom and top layout

---
+++ src/features/layouts/bottom-nav.php
@@ -0,0 +1,19 @@
+<section class="bottom-left-nav">
+    <div class="langs">
+        <a href="/en">ENG</a>
+        <a href="/fr">FR</a>
+        <a href="#">KR</a>
+    </div>
+
+    <!-- dark/light mode toggle
+    <div class="toggle-mode">
+        <img src="" alt="">
+            <a href=""></a>
+        </img>
+    </div> -->
+
+    <div class="socials">
+        <a href="/instagram">INSTAGRAM</a>
+        <a href="/facebook">FACEBOOK</a>
+    </div>
+</section>
