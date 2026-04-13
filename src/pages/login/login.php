78e58217fc49a1c705acddc9d0a66b1ac6d2c2cc
TeddieXII <benard.abigail001@gmail.com>
2026-04-13 11:36:15 +0400

    Note: change link of page

--- src/pages/login/login.php
+++ src/pages/login/login.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
-        header("Location: dashboard.php"); //redirect to protected page
+        header("Location: dashboard.php"); //change link of page
         exit();
     } else {
         $error = "Invalid email or password!";
